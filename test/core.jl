960e30b4b8fce738d4cf0e21a110454e8f85a68f
Rafael Fourquet <fourquet.rafael@gmail.com>
2017-12-01 13:11:28 +0100

    move Random to stdlib

--- test/core.jl
+++ test/core.jl
@@ -1,6 +1,9 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
 # test core language features
+
+using Random
+
 const Bottom = Union{}
 
 # For curmod_*
@@ -4180,7 +4183,7 @@ end
 
 # issue #14113
 module A14113
-    using Test
+    using Test, Random
     # show that making several thousand methods (and lots of AST constants)
     # doesn't cause any serious issues (for example, for the serializer)
     # although to keep runtime on the order of several seconds for this test,
@@ -5524,6 +5527,7 @@ module UnionOptimizations
 
 using Test
 using Dates
+using Random
 
 const boxedunions = [Union{}, Union{String, Nothing}]
 const unboxedunions = [Union{Int8, Nothing},
